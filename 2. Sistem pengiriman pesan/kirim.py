a918cffc87262369c6b31f998e4104ab7e6dab98
prabu-1945 <prabusang531@gmail.com>
2026-05-21 05:21:51 +0700

    revisi 2

--- 2. Sistem pengiriman pesan/kirim.py
+++ 2. Sistem pengiriman pesan/kirim.py
@@ -33,23 +33,3 @@ class HashTableAkun:
 
         return True
 
-    # Mencari akun
-    def cari_akun(self, username):
-
-        indeks = self.hash_username(username)
-
-        indeks_awal = indeks
-
-        while self.tabel[indeks] is not None:
-
-            if self.tabel[indeks][0] == username:
-                return self.tabel[indeks]
-
-            indeks = (indeks + 1) % self.ukuran_tabel
-
-            if indeks == indeks_awal:
-                break
-
-        return None
-
-
